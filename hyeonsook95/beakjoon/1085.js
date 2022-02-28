fcf7303c0da191709d3417c25a684dad1f2dba16
hyeonsook95 <qu3230@gmail.com>
2022-02-28 21:43:55 +0900

    docs: 폴더 변경

--- hyeonsook95/beakjoon/1085.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-var input = fs.readFileSync('dev/stdin').toString().split(' ');
-
-const [x, y, w, h] = input.map(num => parseInt(num));
-console.log(x, y, (w - x), (h - y));
