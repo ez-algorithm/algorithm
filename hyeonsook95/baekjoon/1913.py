089919b4f2ef98807116060a1504225f1329b2cb
hyeonsook95 <qu3230@gmail.com>
2022-03-30 00:01:51 +0900

    fix : 코드 수정

--- hyeonsook95/baekjoon/1913.py
+++ hyeonsook95/baekjoon/1913.py
@@ -3,6 +3,9 @@ import sys
 input = sys.stdin.readline
 
 N = int(input().strip())
+target = int(input().strip())
+
+nr, nc = -1, -1
 maps = [[0 for _ in range(N)] for _ in range(N)]
 
 dr, dc = [1, 0, -1, 0], [0, 1, 0, -1]
@@ -10,6 +13,8 @@ dr, dc = [1, 0, -1, 0], [0, 1, 0, -1]
 d_idx = 0
 r_idx, c_idx = 0, 0
 for num in range(N**2, 0, -1):
+    if num == target:
+        nr, nc = r_idx, c_idx
     maps[r_idx][c_idx] = num
     if not (
         -1 < r_idx + dr[d_idx] < N
@@ -21,4 +26,5 @@ for num in range(N**2, 0, -1):
     c_idx += dc[d_idx]
 
 for m in maps:
-    print(*m, sep=" ", end="\n")
+    print(*m, end="\n")
+print(nr + 1, nc + 1)
