2408612f055ea05bd400013956a2c5eae8223a3b
hyeonsook95 <qu3230@gmail.com>
2022-04-02 14:50:59 +0900

    add : 하현숙 boj 10798 구현 세로읽기

--- hyeonsook95/baekjoon/10798.ts
+++ hyeonsook95/baekjoon/10798.ts
@@ -1,15 +1,4 @@
-const rl = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-let input: string[] = [];
-
-rl.on('line', function (line) {
-    input.push(line);
-}).on('close', function () {
-    const zip: Function = rows => rows[0].map((_, c) => rows.map(row => row[c]));
-    let answer = input.map(chars => (chars.trim().split("").concat(Array(15 - chars.length).fill(' '))));
-    console.log(zip(answer).map(chars => chars.join("").replace(" ", "")).join("").replace(" ", ""));
-    process.exit();
-});
-
+const fs = require("fs");
+const zip = rows => rows[0].map((_, c) => rows.map(row => row[c]));
+var input = fs.readFileSync('dev/stdin').toString().split('\n').map(chars => (chars.trim().split("").concat(Array(15 - chars.length).fill(' '))));
+console.log(zip(input).map(chars => chars.join("")).join("").replace(/(\s*)/g, ''));
