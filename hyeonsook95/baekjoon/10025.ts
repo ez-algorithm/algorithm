1b54360b17510d37c85aa77dfb5ae832d3e1162a
hyeonsook95 <qu3230@gmail.com>
2022-04-05 21:58:13 +0900

    add : 하현숙 boj 10025 슬라이딩 윈도우 게으른 백곰

---
+++ hyeonsook95/baekjoon/10025.ts
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+
+const [N, K] = input[0].split(' ').map(num => parseInt(num));
+var maps = Array(1000001).fill(0);
+
+for (var inputIdx = 0; inputIdx < N; inputIdx++) {
+    var [ice, idx] = input[inputIdx + 1].trim().split(' ').map(num => parseInt(num));
+    maps[idx] = ice;
+}
+
+var result = 0;
+var end = (2 * K + 1) > 1000001 ? 1000001 : (2 * K + 1);
+for (var idx = 0; idx < end; idx++) {
+    result += maps[idx];
+}
+var left = 0;
+var right = 2 * K;
+var pSum = result;
+while (right < 1000000) {
+    left += 1;
+    right += 1;
+
+    pSum = pSum - maps[left - 1] + maps[right];
+    result = pSum > result ? pSum : result;
+}
+
+console.log(result);
