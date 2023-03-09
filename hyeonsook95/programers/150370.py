52a0f53117570164471dcbb3bb6152061f88b4d7
hyeonsook95 <qu3230@gmail.com>
2023-03-09 20:51:19 +0900

    필요없는 lib 없애기

--- hyeonsook95/programers/150370.py
+++ hyeonsook95/programers/150370.py
@@ -1,22 +1,19 @@
-from datetime import datetime
-
 def solution(today, terms, privacies):
     answer = []
     
     dt = {k: int(v) for k, v in map(lambda t: t.split(' '), terms)}
-    today = datetime.strptime(today, '%Y.%m.%d')
     for idx, privacy in enumerate(privacies):
         sd, t = privacy.split(' ')
 
         y, m, d = map(int, sd.split('.'))
-        m += dt[t]
-        y += m // 12
-        m = m % 12
+        dm = m + dt[t]
+        y += dm // 12
+        m = dm % 12
         
-        if m % 12 ==0:
+        if m == 0:
             y -= 1
             m = 12
 
-        if today >= datetime(y, m, d):
+        if today >= f"{y}.{str(m).zfill(2)}.{str(d).zfill(2)}":
             answer.append(idx + 1)
     return answer
